8712c7917e6dfec49c2f369d37197c5fa893d64a
teddyteh <teddymilktea@gmail.com>
2017-04-05 15:00:11 +1000

    First commit

---
+++ Facebook Clone/Facebook Clone/Models/Data/Db.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Facebook_Clone.Models.Data
+{
+    public class Db : DbContext
+    {
+        public DbSet<UserDto> users { get; set; }
+        public DbSet<FriendDto> friends { get; set; }
+    }
+}
